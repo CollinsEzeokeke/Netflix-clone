01987496e7e745c3e50d9076efcff237d2357b8c
Collins Ezeokeke <collinsmecosy@gmail.com>
2025-04-12 08:37:44 +0100

    Comment out Navs component and Home component in App.js for debugging purposes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import React from 'react';
 import Home from "./component/netflix/Pages/Home";
-import Navs from './component/netflix/Pages/Navs';
+// import Navs from './component/netflix/Pages/Navs';
 import Faq from './component/netflix/Pages/faq';
 import Jobs from './component/netflix/Pages/jobs';
 import Privacy from './component/netflix/Pages/privacy';
 function App() {
   return (
     <>
-      <Navs />
+      {/* <Home /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/faq' element={<Faq />} />
